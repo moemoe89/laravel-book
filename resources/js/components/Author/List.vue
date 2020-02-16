be97905a3aa2c6df1ec05181972dbf1a167dffeb
Bismo Baruno <bismobaruno@gmail.com>
2020-02-17 00:10:05 +0700

    adding search and sort to author and book list

--- resources/js/components/Author/List.vue
+++ resources/js/components/Author/List.vue
@@ -5,15 +5,13 @@
             <div class="card-header"><router-link class="float-right btn btn-success btn-sm" to="/author/add"><i class="fa fa-fw fa-plus-circle"></i> Add Author</router-link></div>
             <div class="card-body">
                 <div class="col-sm-12">
-                    <form method="get">
-                        <div class="row">
-                            <div class="col-sm-2">
-                                <div class="form-group">
-                                    <input type="text" name="search" id="search" class="form-control" value="" placeholder="Search">
-                                </div>
+                    <div class="row">
+                        <div class="col-sm-2">
+                            <div class="form-group">
+                                <input type="text" v-model="search" v-on:keyup.enter="searchAuthor" class="form-control" value="" placeholder="Search">
                             </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -21,10 +19,10 @@
         <table class="table table-bordered">
             <thead>
             <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Created At</th>
-                <th>Updated At</th>
+                <th @click="sort('id')">ID</th>
+                <th @click="sort('name')">Name</th>
+                <th @click="sort('created_at')">Created At</th>
+                <th @click="sort('updated_at')">Updated At</th>
                 <th>Actions</th>
             </tr>
             </thead>
@@ -32,8 +30,8 @@
             <tr v-for="author in authors" :key="author.id">
                 <td>{{ author.id }}</td>
                 <td>{{ author.name }}</td>
-                <td>{{ author.created_at }}</td>
-                <td>{{ author.updated_at }}</td>
+                <td>{{ author.created_at | localTime }}</td>
+                <td>{{ author.updated_at | localTime }}</td>
                 <td>
                     <div class="btn-group" role="group">
                         <router-link :to="{name: 'author_edit', params: { id: author.id }}" class="btn btn-primary">Edit
@@ -51,17 +49,29 @@
     export default {
         data() {
             return {
-                authors: []
+                authors: [],
+                search: (this.$route.query.search || ''),
+                currentSort: (this.$route.query.order_by || ''),
+                currentSortDir: (this.$route.query.sort || '')
             }
         },
         created() {
-            this.axios
-                .get(window.location.origin+'/api/v1/author?search='+(this.$route.query.search || ''))
-                .then(response => {
-                    this.authors = response.data.data;
-                });
+            this.list();
         },
         methods: {
+            list() {
+                this.axios
+                    .get(window.location.origin+'/api/v1/author', {
+                      params: {
+                        search: this.search,
+                        order_by: this.currentSort,
+                        sort: this.currentSortDir,
+                      },
+                    })
+                    .then(response => {
+                        this.authors = response.data.data;
+                    });
+            },
             deleteAuthor(id) {
                 if(confirm("Do you really want to delete?")){
                     this.axios
@@ -71,6 +81,36 @@
                             this.authors.splice(i, 1)
                         });
                 }
+            },
+            searchAuthor:function() {
+                this.list();
+                this.search = this.search;
+                this.$router.replace({ name: "home", query: {search: this.search, order_by: this.currentSort, sort: this.currentSortDir} });
+            },
+            sort:function(s) {
+                //if s == current sort, reverse
+                if(s === this.currentSort) {
+                    this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+                }
+                this.currentSort = s;
+                this.list();
+                this.$router.replace({ name: "home", query: {search: this.search, order_by: this.currentSort, sort: this.currentSortDir} });
+            }
+        },
+        computed:{
+            authors:function() {
+                return this.authors.sort((a,b) => {
+                    let modifier = 1;
+                    if(this.currentSortDir === 'desc') modifier = -1;
+                    if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+                    if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+                    return 0;
+                });
+            }
+        },
+        filters: {
+            localTime: function (date) {
+               return moment(date + ' Z', 'YYYY-MM-DD HH:mm:ss Z', true).format('D MMM YYYY HH:mm');
             }
         }
     }
--- resources/js/components/Book/List.vue
+++ resources/js/components/Book/List.vue
@@ -5,15 +5,13 @@
             <div class="card-header"><router-link class="float-right btn btn-success btn-sm" to="/book/add"><i class="fa fa-fw fa-plus-circle"></i> Add Book</router-link></div>
             <div class="card-body">
                 <div class="col-sm-12">
-                    <form method="get">
-                        <div class="row">
-                            <div class="col-sm-2">
-                                <div class="form-group">
-                                    <input type="text" name="search" id="search" class="form-control" value="" placeholder="Search">
-                                </div>
+                    <div class="row">
+                        <div class="col-sm-2">
+                            <div class="form-group">
+                                <input type="text" v-model="search" v-on:keyup.enter="searchBook" class="form-control" value="" placeholder="Search">
                             </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -21,11 +19,11 @@
         <table class="table table-bordered">
             <thead>
             <tr>
-                <th>ID</th>
-                <th>Author</th>
-                <th>Title</th>
-                <th>Created At</th>
-                <th>Updated At</th>
+                <th @click="sort('id')">ID</th>
+                <th @click="sort('name')">Author</th>
+                <th @click="sort('title')">Title</th>
+                <th @click="sort('created_at')">Created At</th>
+                <th @click="sort('updated_at')">Updated At</th>
                 <th>Actions</th>
             </tr>
             </thead>
@@ -34,8 +32,8 @@
                 <td>{{ book.id }}</td>
                 <td>{{ book.name }}</td>
                 <td>{{ book.title }}</td>
-                <td>{{ book.created_at }}</td>
-                <td>{{ book.updated_at }}</td>
+                <td>{{ book.created_at | localTime }}</td>
+                <td>{{ book.updated_at | localTime }}</td>
                 <td>
                     <div class="btn-group" role="group">
                         <router-link :to="{name: 'book_edit', params: { id: book.id }}" class="btn btn-primary">Edit
@@ -53,17 +51,29 @@
     export default {
         data() {
             return {
-                books: []
+                books: [],
+                search: (this.$route.query.search || ''),
+                currentSort: (this.$route.query.order_by || ''),
+                currentSortDir: (this.$route.query.sort || '')
             }
         },
         created() {
-            this.axios
-                .get(window.location.origin+'/api/v1/book?search='+(this.$route.query.search || ''))
-                .then(response => {
-                    this.books = response.data.data;
-                });
+            this.list();
         },
         methods: {
+            list() {
+                this.axios
+                    .get(window.location.origin+'/api/v1/book', {
+                      params: {
+                        search: this.search,
+                        order_by: this.currentSort,
+                        sort: this.currentSortDir,
+                      },
+                    })
+                    .then(response => {
+                        this.books = response.data.data;
+                    });
+            },
             deleteBook(id) {
                 if(confirm("Do you really want to delete?")){
                     this.axios
@@ -73,6 +83,36 @@
                             this.books.splice(i, 1)
                         });
                 }
+            },
+            searchBook:function() {
+                this.list();
+                this.search = this.search;
+                this.$router.replace({ name: "book", query: {search: this.search, order_by: this.currentSort, sort: this.currentSortDir} });
+            },
+            sort:function(s) {
+                //if s == current sort, reverse
+                if(s === this.currentSort) {
+                    this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+                }
+                this.currentSort = s;
+                this.list();
+                this.$router.replace({ name: "book", query: {search: this.search, order_by: this.currentSort, sort: this.currentSortDir} });
+            }
+        },
+        computed:{
+            books:function() {
+                return this.books.sort((a,b) => {
+                    let modifier = 1;
+                    if(this.currentSortDir === 'desc') modifier = -1;
+                    if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+                    if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+                    return 0;
+                });
+            }
+        },
+        filters: {
+            localTime: function (date) {
+               return moment(date + ' Z', 'YYYY-MM-DD HH:mm:ss Z', true).format('D MMM YYYY HH:mm');
             }
         }
     }
